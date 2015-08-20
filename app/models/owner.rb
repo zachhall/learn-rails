1be0e0587f03dce8a2b4d896f112bd36ca96aa90
zachhall <zachhallnc@gmail.com>
2015-08-19 20:27:54 -0400

    build Owners scaffold

---
+++ app/models/owner.rb
@@ -0,0 +1,23 @@
+class Owner
+	
+	def name
+		name = 'Zach'
+	end
+
+	def birthdate
+		birthdate = Date.new(1990, 9, 17)
+	end
+
+	def countdown
+		today = Date.today
+
+		birthday = Date.new(today.year, birthdate.month, birthdate.day)
+
+		if birthday > today
+			countdown = (birthday - today).to_i
+		else
+			countdown = (birthday.next_year - today).to_i
+		end
+	end
+
+end
